4f28a9046c1564cd061eb33679cf9958c225d750
n7700 <n7700145@gmail.com>
2024-07-28 20:34:59 -0400

    Update tetrisarrmanagement.py

--- tetrisarrmanagement.py
+++ tetrisarrmanagement.py
@@ -3,9 +3,15 @@ from queue import Queue
 
 class Arrmanage():
     def __init__(self):
+
+        #setting up next queue
+        
         self.queue = Queue()
         for i in range(3):
             self.getpiece()
+
+        #setting up initial 2d array for board
+        
         self.arr = []
         for i in range(20):
             x=[]
@@ -13,6 +19,9 @@ class Arrmanage():
                 x.append(None)
             self.arr.append(x)
         self.temparr = self.arr
+
+        #init shapes
+        
         xreg = 'X'
 
         shape1 = [
@@ -69,24 +78,32 @@ class Arrmanage():
     def checkmove(self):
         f = True
         for i in range(1,len(self.arr)):
+
+            #checking if previous not empty
             x = False
             if f == False:
                 x = True
+
+            #checking if line is empty
             f = True
             for j in self.arr[i]:
                 if j != None:
                     f = False
+
+            #if line empty and line above not, drop above line
             if f == True and x == True:
                 self.arr[i] = self.arr[i-1]
                 self.arr[i-1] = [None,None,None,None,None,None,None,None,None,None]
                 self.checkmove()
                 
     def checkempty(self):
+        #clears line
         for i in range(1,len(self.arr)):
             if None not in self.arr[i]:
                 self.arr[i] = [None,None,None,None,None,None,None,None,None,None]
                 self.checkmove()
     def getpiece(self):
+        #takes random piece and adds it to the queue
         p = random.randrange(0,7)
         shape = self.shapes[p]
         self.queue.inqueue(shape)
