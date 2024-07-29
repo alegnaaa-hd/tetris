f836b046da7513b2783e0250055e69e9dcfbe4bb
Angela Wang <angelavveivvei@gmail.com>
2024-07-28 20:17:28 -0400

    whitespace cause code = readable now

--- tetrisarrmanagement.py
+++ tetrisarrmanagement.py
@@ -8,49 +8,58 @@ class Arrmanage():
             self.arr.append(x)
         self.temparr = self.arr
         xreg = 'X'
+
         shape1 = [
             [None,None,None,None]
             [None,None,None,None]
             [None,None,None,None]
             [xreg,xreg,xreg,xreg]
             ]
+        
         shape2 = [
             [None,None,None,None]
             [None,None,None,None]
             [xreg,None,None,None]
             [xreg,xreg,xreg,None]
             ]
+        
         shape3 = [
             [None,None,None,None]
             [None,None,None,None]
             [None,None,xreg,None]
             [xreg,xreg,xreg,None]
             ]
+        
         shape4 = [
             [None,None,None,None]
             [None,None,None,None]
             [xreg,xreg,None,None]
             [xreg,xreg,None,None]
             ]
+        
         shape5 = [
             [None,None,None,None]
             [None,None,None,None]
             [None,xreg,xreg,None]
             [xreg,xreg,None,None]
             ]
+        
         shape6 = [
             [None,None,None,None]
             [None,None,None,None]
             [None,xreg,None,None]
             [xreg,xreg,xreg,None]
             ]
+        
         shape7 = [
             [None,None,None,None]
             [None,None,None,None]
             [xreg,xreg,None,None]
             [None,xreg,xreg,None]
             ]
+        
         self.shapes = [shape1,shape2,shape3,shape4,shape5,shape6,shape7]
+        
     def checkmove(self):
         f = True
         for i in range(1,len(self.arr)):
@@ -65,6 +74,7 @@ class Arrmanage():
                 self.arr[i] = self.arr[i-1]
                 self.arr[i-1] = [None,None,None,None,None,None,None,None,None,None]
                 self.checkmove()
+                
     def checkempty(self):
         for i in range(1,len(self.arr)):
             if None not in self.arr[i]:
