4ab7e0069b253aaffdf0445c60d191f3a530daa8
angela wang <140568623+alegnaaa-hd@users.noreply.github.com>
2024-07-31 10:08:43 -0400

    Update credits.py

--- credits.py
+++ credits.py
@@ -15,6 +15,7 @@ def credits():
 
   Big_game_font = pygame.font.Font(font_path, 40) 
   game_font = pygame.font.Font(font_path, 20)
+  small_font = pygame.font.Font(font_path, 17)
   
    # main credits banner 
   credits_text = Big_game_font.render("Credits", True, "white")
@@ -40,26 +41,29 @@ def credits():
   names2_text = game_font.render("Nick Scott, and Rachael Wang", True, "white")
   screen.blit(names2_text, (245, 260))
 
-  # insert music credits
-  music_text = game_font.render("insert music credits", True, "white")
-  screen.blit(music_text, (335, 300))
+  # music credits
+  music_text = game_font.render("Background Music:", True, "white")
+  screen.blit(music_text, (310, 300))
+
+  musicLink_text = small_font.render("https://freesound.org/people/ironreaper1832/sounds/710963/", True, "white")
+  screen.blit(musicLink_text, (70, 340))
   
   # link to the code 
   codeLink_text = game_font.render("Code Link: https://github.com/alegnaaa-hd/tetris", True, "white")
-  screen.blit(codeLink_text, (145, 340))
+  screen.blit(codeLink_text, (145, 380))
   
   # project description (?)
   description_text = game_font.render("Data Structris is our team's recreation of the popular", True, "white")
-  screen.blit(description_text, (45, 410))
+  screen.blit(description_text, (45, 440))
 
   description2_text = game_font.render("arcade game: Tetris. With all of the classic game features ", True, "white")
-  screen.blit(description2_text, (25, 450))
+  screen.blit(description2_text, (25, 480))
 
   description3_text = game_font.render("like rotating, holding, and placing colored shapes, our ", True, "white")
-  screen.blit(description3_text, (45, 490))
+  screen.blit(description3_text, (45, 520))
 
   description4_text = game_font.render(" game is fun, frustration, and entertainment all in one!", True, "white")
-  screen.blit(description4_text, (35, 530))
+  screen.blit(description4_text, (35, 560))
   
   # back button
   back_img = pygame.image.load("images/button_back.png").convert_alpha()
@@ -73,11 +77,16 @@ def credits():
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
             print(f'{x}, {y}')
+            # link to music
+            if x > 66 and x < 800:
+              if y > 345 and y < 360:
+                webbrowser.open(r"https://freesound.org/people/ironreaper1832/sounds/710963/")
+            
             # link to github
-            if x > 285 and x < 760:
-              if y > 345 and y < 370:
+            if x > 300 and x < 840:
+              if y > 385 and y < 400:
                 webbrowser.open(r"https://github.com/alegnaaa-hd/tetris")
-                
+    
             # back button
             if x > 290 and x < 580:
               if y > 620 and y < 715:
