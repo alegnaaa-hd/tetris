65e0ea9d8d2f347331d7e694f480981f75d0b451
angela wang <140568623+alegnaaa-hd@users.noreply.github.com>
2024-07-31 09:43:46 -0400

    Update credits.py

--- credits.py
+++ credits.py
@@ -1,54 +1,69 @@
 import pygame
 import button
 from pygame.locals import QUIT
+import webbrowser
 
 def credits():
   from main import main_menu
   pygame.init()
   screen = pygame.display.set_mode((880, 1000))
 
-  Bigfont = pygame.font.SysFont("timesnewroman", 36)
-  font = pygame.font.SysFont("timesnewroman", 25)
+  #Bigfont = pygame.font.SysFont("timesnewroman", 36)
+  #font = pygame.font.SysFont("timesnewroman", 25)
+
+  font_path = 'fonts.ttf'
+
+  Big_game_font = pygame.font.Font(font_path, 40) 
+  game_font = pygame.font.Font(font_path, 20)
   
    # main credits banner 
-  credits_text = Bigfont.render("Credits", True, "white")
+  credits_text = Big_game_font.render("Credits", True, "white")
   screen.blit(credits_text, (365, 40))
 
   # data structures final project
-  dsaa_text = font.render("Data Structures And Algorithms (DSAA/DATA)", True, "white")
+  dsaa_text = game_font.render("Data Structures And Algorithms (DSAA/DATA)", True, "white")
   screen.blit(dsaa_text, (160, 100))
 
   # final project
-  dsaa_text = font.render("Final Project", True, "white")
+  dsaa_text = game_font.render("Final Project", True, "white")
   screen.blit(dsaa_text, (350, 140))
   
   # cty car 24.2 @ Dickinson College
-  cty_text = font.render("CTY CAR 24.2 @ Dickinson College", True, "white")
+  cty_text = game_font.render("CTY CAR 24.2 @ Dickinson College", True, "white")
   screen.blit(cty_text, (220, 180))
   
   # angela wang, kaylin han, nick scott, rachael wang
 
-  names_text = font.render("CREATORS: Angela Wang, Kaylin Han, ", True, "white")
+  names_text = game_font.render("CREATORS: Angela Wang, Kaylin Han, ", True, "white")
   screen.blit(names_text, (200, 220))
 
-  names2_text = font.render("Nick Scott, and Rachael Wang", True, "white")
+  names2_text = game_font.render("Nick Scott, and Rachael Wang", True, "white")
   screen.blit(names2_text, (245, 260))
 
   # insert music credits
-  music_text = font.render("insert music credits", True, "white")
+  music_text = game_font.render("insert music credits", True, "white")
   screen.blit(music_text, (335, 300))
   
-  # link to the code maybe
-  codeLink_text = font.render("insert code link(?)", True, "white")
-  screen.blit(codeLink_text, (335, 340))
+  # link to the code 
+  codeLink_text = game_font.render("Code Link: https://github.com/alegnaaa-hd/tetris", True, "white")
+  screen.blit(codeLink_text, (145, 340))
   
   # project description (?)
-  description_text = font.render("insert description here", True, "white")
-  screen.blit(description_text, (325, 380))
+  description_text = game_font.render("Data Structris is our team's recreation of the popular", True, "white")
+  screen.blit(description_text, (45, 410))
+
+  description2_text = game_font.render("arcade game: Tetris. With all of the classic game features ", True, "white")
+  screen.blit(description2_text, (25, 450))
+
+  description3_text = game_font.render("like rotating, holding, and placing colored shapes, our ", True, "white")
+  screen.blit(description3_text, (45, 490))
+
+  description4_text = game_font.render(" game is fun, frustration, and entertainment all in one!", True, "white")
+  screen.blit(description4_text, (35, 530))
   
   # back button
   back_img = pygame.image.load("images/button_back.png").convert_alpha()
-  back_button = button.Button(290, 420, back_img, 0.25)
+  back_button = button.Button(290, 620, back_img, 0.25)
   back_button.draw(screen)
 
   while True:
@@ -58,8 +73,14 @@ def credits():
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
             print(f'{x}, {y}')
+            # link to github
+            if x > 285 and x < 760:
+              if y > 345 and y < 370:
+                webbrowser.open(r"https://github.com/alegnaaa-hd/tetris")
+                
+            # back button
             if x > 290 and x < 580:
-              if y > 420 and y < 515:
+              if y > 620 and y < 715:
                 main_menu()
                 
         elif event.type == QUIT:
